ef51dd5f0fa804fa2a2ee983c33216899eb874ef
anovgorod <novgorodsev@gmail.com>
2025-05-19 01:30:51 +0700

    rename club again!*)))

--- posts/rss.py
+++ posts/rss.py
@@ -4,7 +4,7 @@ from posts.models.post import Post
 
 
 class NewPostsRss(Feed):
-    title = "BRNDS: Новые посты"
+    title = "NVA Club: Новые посты"
     link = "/posts.rss"
     description = ""
     limit = 20
